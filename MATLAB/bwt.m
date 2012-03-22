722a41924905d6e32bdd6a3755d47d843c3a4c20
Juan Carlos Galan <jcgalanh@gmail.com>
2012-03-21 22:06:32 -0600

    Added g1 bwt

---
+++ MATLAB/bwt.m
@@ -0,0 +1,6 @@
+function [ output_args ] = bwt( signal, level, name )
+%BWT Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
---
+++ MATLAB/bwtg1.m
@@ -0,0 +1,7 @@
+function [ O E ] = bwtg1( sO, sE )
+%BWTG1 Summary of this function goes here
+%   Detailed explanation goes here
+E = bitxor(sO,sE);
+O = sO;
+end
+
