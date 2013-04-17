996d500568a60f18fc21f08af34c6f5fc40f7243
Juan Carlos Galan <jcgalanh@gmail.com>
2013-04-17 14:13:23 -0500

    Adding GUI with QT

---
+++ python/GUI/GUITest.py
@@ -0,0 +1,32 @@
+'''
+Created on 17/04/2013
+
+@author: JuanCarlos
+'''
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+class Form(QDialog):
+    def __init__(self, parent = None):
+        super(Form,self).__init__(parent)
+        
+        dial = QDial()
+        dial.setNotchesVisible(True)
+        
+        spinbox = QSpinBox()
+        
+        layout = QHBoxLayout()
+        layout.addWidget(dial)
+        layout.addWidget(spinbox)
+        self.setLayout(layout)
+        
+        self.connect(dial,SIGNAL("valueChanged(int)"), spinbox.setValue)
+        self.connect(spinbox,SIGNAL("valueChanged(int)"), dial.setValue)
+        
+        self.setWindowTitle("Signals")
+        
+app = QApplication(sys.argv)
+form = Form()
+form.show()
+app.exec_()
