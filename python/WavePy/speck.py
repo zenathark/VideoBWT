139b3226022602619f09f8258341282566e3ed0c
Juan Carlos Galan <jcgalanh@gmail.com>
2013-04-22 22:31:47 -0500

    Coded ProcesS

--- python/WavePy/speck.py
+++ python/WavePy/speck.py
@@ -14,26 +14,47 @@ class speck(object):
     S = []
     I = []
     n = 0
+    output = []
 
     def __init__(self, wavelet):
-        wv = wavelet
-        dt = wavelet.data
-        LIS = CircularStack(wv.cols * wv.rows)
-        LSP = CircularStack(wv.cols * wv.rows)
+        self.wv = wavelet
+        self.dt = wavelet.data
         
     def initialization(self):
         X = self.wv.get_morton_order(self.wv.rows * self.wv.cols)
-        self.LSP = []
-        s_size = (self.wv.row / 2**wv.level * self.wv.cols/ 2**wv.level)
+        self.LIS = CircularStack(self.wv.cols * self.wv.rows)
+        self.LSP = CircularStack(self.wv.cols * self.wv.rows)
+        s_size = (self.wv.row / 2**self.wv.level * self.wv.cols/ 2**self.wv.level)
         self.S = X[:s_size-1]
         del X[:s_size-1]
-        I = X
+        self.I = X
         maxs = abs(self.wavelet.data)
         self.n = int(math.log(maxs.max(),2))
-        self.LIS = []
-        self.LIS += [(self.S)]
+        self.LIS.push(self.S)
 
+    def S_n(self,S):
+        pass
 
-    def compress(self):
+    def ProcessS(self,S):
+        sn = self.S_n(S)
+        self.output += [sn]
+        if sn == 1:
+            if len(S) == 1:
+                self.output += [sign(S)]
+                self.LSP.push(S)
+                #TODO erase S
+            else:
+                self.CodeS(S)
+        #TODO add S
+    
+    def CodeS(self,S):
+        pass
 
+    def ProcessI(self):
+        pass
 
+    def CodeI(self):
+        pass
+    
+    def sign(self,S):
+        pass
